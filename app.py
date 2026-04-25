960f758a3811a0af8a6770a176e2d904a3ea32cf
Md Monser Ali <81038623+MonserALi@users.noreply.github.com>
2026-04-25 17:48:42 +0600

    Add files via upload

--- app.py
+++ app.py
@@ -3,23 +3,6 @@ from google import genai
 import os
 from api_calling import generate_response
 
-clicked = False
-
-if clicked:
-
-    if not text:
-        st.error("Please enter some text ")
-    if not selected:
-        st.error("Please select your desire language")
-
-    if text and selected:
-        with st.container(border=True):
-            with st.spinner("Translating..."):
-                response = generate_response(text,selected)
-                st.markdown(response)
-
-                
-
 
 
 st.header("Easy Translator",anchor=None)
@@ -35,4 +18,18 @@ clicked  = st.button("Translate",type='primary')
 st.set_page_config(page_title="Translator", layout="wide")
 st.divider()
 
+if clicked:
+
+    if not text:
+        st.error("Please enter some text ")
+    if not selected:
+        st.error("Please select your desire language")
+
+    if text and selected:
+        with st.container(border=True):
+            with st.spinner("Translating..."):
+                response = generate_response(text,selected)
+                st.markdown(response)
+
+                
 
