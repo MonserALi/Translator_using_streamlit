47095a2eb5ee3e6218861061782664f483b803b8
Md Monser Ali <81038623+MonserALi@users.noreply.github.com>
2026-04-25 17:15:41 +0600

    Add files via upload

---
+++ app1.py
@@ -0,0 +1,36 @@
+import streamlit as st
+from google import genai
+import os
+from api_calling import generate_response
+
+
+
+st.header("Controls ",anchor=None)
+text = st.text_input("Enter Sentence")
+    
+
+selected = st.selectbox("Translate to ",
+        ('Bangla','English','Korean'),
+        index=2
+)
+clicked  = st.button("Translate",type='primary')
+
+st.set_page_config(page_title="Translator", layout="wide")
+st.text("Enter your text and hit on the button to translate")
+st.divider()
+
+if clicked:
+
+    if not text:
+        st.error("Please enter some text ")
+    if not selected:
+        st.error("Please select your desire language")
+
+    if text and selected:
+        with st.container(border=True):
+            with st.spinner("Translating..."):
+                response = generate_response(text,selected)
+                st.markdown(response)
+
+                
+
