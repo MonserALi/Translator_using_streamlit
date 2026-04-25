1b44af2659fbcb0c94e61d5f3f1b9b0ec5eac9fc
Md Monser Ali <81038623+MonserALi@users.noreply.github.com>
2026-04-25 19:37:51 +0600

    Add files via upload

--- app.py
+++ app.py
@@ -7,17 +7,18 @@ from api_calling import  generate_response,audio_transcription
 response = ""
 
 st.header("Easy Translator",anchor=None)
-text = st.text_input("",placeholder="Enter your words or sentences")
-    
 
-selected = st.selectbox("Translate to ",
-        ('Bangla','English','Korean','Hindi','Urdu'),
-        index=2
-)
-clicked  = st.button("Translate",type='primary')
+with st.form(key="my_form",  enter_to_submit=True):
+
+    text = st.text_input("Text in any language",placeholder="Enter your words or sentences")
+      
+    selected = st.selectbox("Translate to ",
+            ('Bangla','English','Korean','Hindi','Urdu'),
+            index=2
+    )
+    clicked  = st.form_submit_button("Translate",type="primary")
 
 st.set_page_config(page_title="Translator", layout="wide")
-st.divider()
 
 if clicked:
 
@@ -28,6 +29,7 @@ if clicked:
 
     if text and selected:
         with st.container(border=True):
+            st.subheader(f"Your text in {selected}")
             with st.spinner("Translating..."):
                 response = generate_response(text,selected)
                 st.markdown(response)
